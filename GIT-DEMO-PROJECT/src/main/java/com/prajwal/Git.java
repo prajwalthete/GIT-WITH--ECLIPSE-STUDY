c357e1739bd9127fdade874796399d9bf7721be2
prajwal <prajwalthete98@gmail.com>
2022-12-27 11:12:21 +0530

    initial commit from second branch

--- GIT-DEMO-PROJECT/src/main/java/com/prajwal/Git.java
+++ GIT-DEMO-PROJECT/src/main/java/com/prajwal/Git.java
@@ -5,6 +5,7 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("Hello From GitHub demo");
 		System.out.println("CCE IS COMING MAN STUDY HARD NOW ");
+		System.out.println("hello From New Branch");
 		// TODO Auto-generated method stub
 
 	}
